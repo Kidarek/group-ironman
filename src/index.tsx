cbcbc76471129081823f81cc62ba8ee8df559b79
Shane <evilace0@gmail.com>
2021-10-16 19:41:10 -0400

    added some styling

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,25 @@ import * as React from 'react'
 import * as ReactDOM from "react-dom"
 import './index.css'
 import App from './App'
+import { createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@emotion/react';
+import { Box } from '@mui/system';
+import { CssBaseline } from '@mui/material';
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme = {darkTheme}>
+      <CssBaseline/>
+      <Box>
+        <App />
+      </Box>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
